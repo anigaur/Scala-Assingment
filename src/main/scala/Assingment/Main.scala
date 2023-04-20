6837000344789cb21b355e780eccc74c79bf1e03
anigaur <anigaur@deloitte.com>
2023-04-20 23:29:53 +0530

    MileStone 3 Added

--- src/main/scala/Assingment/Main.scala
+++ src/main/scala/Assingment/Main.scala
@@ -1,7 +1,5 @@
 package Assingment
 
-import sun.tools.jstatd.RemoteVmImpl
-
 import scala.io.Source
 import scala.util.control.*
 import scala.collection.mutable.ListBuffer
@@ -10,22 +8,16 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    case class Movies(imdb_title_id1: String, title1: String, year1: String, drct: String, cntry: String, lang: String, bdgt: String, durtn: String, usrRvws: String) {
+    case class Movies(imdb_title_id1: String, title1: String, year1: String, drct: String, cntry: List[String], lang: String, bdgt: String, durtn: String, usrRvws: String) {
       var imdb_title_id: String = imdb_title_id1
       var title: String = title1
       var year: Int = year1.toInt
       var director: String = drct
-      var country: String = cntry
+      var country: List[String] = cntry
       var language: String = lang
-      var Budget: String = bdgt
+      var budget: Int = bdgt.filter(_.isDigit).toInt
       var duration: Int = durtn.toInt
       var userReviews: Int = usrRvws.toInt
-//      if (usrRvws == "null") {
-//        var userReviews: Int = usrRvws.toInt
-//      }
-
-      // country, minimumVotes,budget,language, reviews from user,duration
-      def printMovie(m: Movies) = println(m.imdb_title_id + " " + m.title + " " + m.year)
     }
 
 
@@ -33,6 +25,7 @@ object Main {
     var i = 0
     var movieIndex = 0
     var movieArray = new Array[Movies](10000)
+    var movieList = new ListBuffer[Movies]()
     var loop = new Breaks
 
     def countWithFilter(string: String, char: Char): Int = string.filter(_ == char).size
@@ -47,15 +40,16 @@ object Main {
 
             var tempList = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)").toList
             var list2 = tempList.map { e => if (e == "") "0" else e }
+            var country = list2(7).replace(" ", "")
+            var countryList = country.replace("\"", "").split(",").toList
             if (list2.size >= 20) {
-              var movie = new Movies(list2(0), list2(1), list2(3), list2(9), list2(8), list2(7), list2(16), list2(6), list2(20))
+              var movie = new Movies(list2(0), list2(1), list2(3), list2(9), countryList, list2(8), list2(16), list2(6), list2(20))
               movieArray(movieIndex) = movie
+              movieList += movie
               movieIndex += 1
 
             }
-
           }
-
         }
         else {
           loop.break
@@ -64,38 +58,58 @@ object Main {
       }
 
     }
-    //    var temp=1
-    //    for(movieElement <- movieArray){
-    //      println( s"${temp} : ${movieElement}")
-    //      temp+=1
-    //    }
+
 
     // milestone 1
     def titleByDirector(director: String, lowerLimit: Int, upperLimit: Int): ListBuffer[String] = {
       var titles = ListBuffer[String]()
       for (movieElemnt <- movieArray) {
-        if ((movieElemnt.director.contains(director))&&(movieElemnt.year >= lowerLimit)&&(movieElemnt.year <= upperLimit )) {
-          titles+= movieElemnt.title
+        if ((movieElemnt.director.contains(director)) && (movieElemnt.year >= lowerLimit) && (movieElemnt.year <= upperLimit)) {
+          titles += movieElemnt.title
 
         }
       }
       return titles
     }
 
-   var titleList =   titleByDirector("Alfred E. Green", 1930, 2000)
-//    println(titleList.toString())
+    var titleList = titleByDirector("Alfred E. Green", 1930, 2000)
+    //    println(titleList.toString())
 
-    def moviesWithHighestReview(review:Int): ListBuffer[String] ={
+    //milestone 2
+
+    def moviesWithHighestReview(review: Int): ListBuffer[String] = {
       var titles = ListBuffer[String]()
-      for (movieElemnt <- movieArray) {
+      movieList = movieList.sortBy(_.userReviews).reverse
+      for (movieElemnt <- movieList) {
         if (movieElemnt.userReviews > review) {
-          titles+= movieElemnt.title
+          titles += movieElemnt.title
         }
       }
       return titles
     }
+
     var titleByReview = moviesWithHighestReview(2)
-//    println(titleByReview)
+    //    println(titleByReview)
+
+    //  milestone3
+
+    var countrySet = Set[String]()
+    for (mapElemnt <- movieList) {
+      countrySet = countrySet.++(mapElemnt.country.toSet)
+    }
+
+    def moviesByCountry(): Unit = {
+
+      for (country <- countrySet) {
+        //      var mov = movieList.groupBy(c => c.country(c.country.indexOf(country)))
+        var mov = movieList.filter(_.country.contains(country))
+        mov = mov.sortBy(b => b.budget).reverse
+        println(s"${country} =>  ${mov(0).budget} ${mov(0).title}")
+        println("-----------------------------------------------------------------")
+      }
+    }
+
+    moviesByCountry()
 
   }
 }
