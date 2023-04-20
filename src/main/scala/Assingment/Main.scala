509504cc94bea90738f813b5bb7be54287c40f8f
anigaur <anigaur@deloitte.com>
2023-04-21 01:13:24 +0530

    Milestone 5 added

--- src/main/scala/Assingment/Main.scala
+++ src/main/scala/Assingment/Main.scala
@@ -8,13 +8,13 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    case class Movies(imdb_title_id1: String, title1: String, year1: String, drct: String, cntry: List[String], lang: String, bdgt: String, durtn: String, usrRvws: String, votes: String) {
+    case class Movies(imdb_title_id1: String, title1: String, year1: String, drct: String, cntry: List[String], lang: List[String], bdgt: String, durtn: String, usrRvws: String, votes: String) {
       var imdb_title_id: String = imdb_title_id1
       var title: String = title1
       var year: Int = year1.toInt
       var director: String = drct
       var country: List[String] = cntry
-      var language: String = lang
+      var language: List[String] = lang
       var budget: Int = bdgt.filter(_.isDigit).toInt
       var duration: Int = durtn.toInt
       var userReviews: Int = usrRvws.toInt
@@ -42,8 +42,10 @@ object Main {
             var list2 = tempList.map { e => if (e == "") "0" else e }
             var country = list2(7).replace(" ", "")
             var countryList = country.replace("\"", "").split(",").toList
+            var language = list2(8).replace(" ", "")
+            var languageList = language.replace("\"", "").split(",").toList
             if (list2.size >= 20) {
-              var movie = new Movies(list2(0), list2(1), list2(3), list2(9), countryList, list2(8), list2(16), list2(6), list2(20), list2(15))
+              var movie = new Movies(list2(0), list2(1), list2(3), list2(9), countryList, languageList, list2(16), list2(6), list2(20), list2(15))
               movieArray(movieIndex) = movie
               movieList += movie
               movieIndex += 1
@@ -94,10 +96,13 @@ object Main {
     //  milestone3
 
     var countrySet = Set[String]()
+    var languageSet = Set[String]()
     for (mapElemnt <- movieList) {
       countrySet = countrySet.++(mapElemnt.country.toSet)
+      languageSet = languageSet.++(mapElemnt.language.toSet)
     }
 
+    //  println(laguageSet.toList.sortBy(r=>r))
     def moviesByCountry(): Unit = {
 
       for (country <- countrySet) {
@@ -118,14 +123,29 @@ object Main {
       for (country <- countrySet) {
         var mov = movieList.filter(_.country.contains(country)).filter(_.vote >= minVotes)
         mov = mov.sortBy(b => b.duration).reverse
-        finalList+= mov(0)
+        finalList += mov(0)
       }
       return finalList
     }
 
     var longestMovieList = longestMovieCountryWise(100)
-//    longestMovieList.foreach(println)
+    //    longestMovieList.foreach(println)
+
+    //MileStone 5
+
+    def languageWiseMovieCount(country: String, minBudget: Int, maxBudget: Int): ListBuffer[Movies] = {
+      var finalList = ListBuffer[Movies]()
+      var movv = movieList.filter(_.country.contains(country))
+      for (language <- languageSet) {
+        var mov = movv.filter(_.language.contains(language)).filter(_.budget >= minBudget).filter(_.budget <= maxBudget)
+        finalList.++(mov)
+        println(s"${language} ${mov.length}")
+      }
+        return finalList
+
+
+    }
+    var temp = languageWiseMovieCount("USA", 1000, 900000000)
+    println(temp)
   }
-
-
 }
