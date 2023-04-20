4165f5882e8e59ddb3d602a4821f5b0b1165bddd
anigaur <anigaur@deloitte.com>
2023-04-21 00:04:01 +0530

    Milestone 4 Added

--- src/main/scala/Assingment/Main.scala
+++ src/main/scala/Assingment/Main.scala
@@ -8,7 +8,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    case class Movies(imdb_title_id1: String, title1: String, year1: String, drct: String, cntry: List[String], lang: String, bdgt: String, durtn: String, usrRvws: String) {
+    case class Movies(imdb_title_id1: String, title1: String, year1: String, drct: String, cntry: List[String], lang: String, bdgt: String, durtn: String, usrRvws: String, votes: String) {
       var imdb_title_id: String = imdb_title_id1
       var title: String = title1
       var year: Int = year1.toInt
@@ -18,6 +18,7 @@ object Main {
       var budget: Int = bdgt.filter(_.isDigit).toInt
       var duration: Int = durtn.toInt
       var userReviews: Int = usrRvws.toInt
+      var vote: Int = votes.toInt
     }
 
 
@@ -43,7 +44,7 @@ object Main {
             var country = list2(7).replace(" ", "")
             var countryList = country.replace("\"", "").split(",").toList
             if (list2.size >= 20) {
-              var movie = new Movies(list2(0), list2(1), list2(3), list2(9), countryList, list2(8), list2(16), list2(6), list2(20))
+              var movie = new Movies(list2(0), list2(1), list2(3), list2(9), countryList, list2(8), list2(16), list2(6), list2(20), list2(15))
               movieArray(movieIndex) = movie
               movieList += movie
               movieIndex += 1
@@ -104,16 +105,28 @@ object Main {
         //      var mov = movieList.groupBy(c => c.country(c.country.indexOf(country)))
         var mov = movieList.filter(_.country.contains(country))
         mov = mov.sortBy(b => b.budget).reverse
-        println(s"${country} =>  ${mov(0).budget} ${mov(0).title}")
-        println("-----------------------------------------------------------------")
+        //        println(s"${country} =>  ${mov(0).budget} ${mov(0).title}")
+        //        println("-----------------------------------------------------------------")
       }
     }
 
     moviesByCountry()
 
+    //    Milestone 4
+
+    def longestMovieCountryWise(minVotes: Int): ListBuffer[Movies] = {
+      var finalList = ListBuffer[Movies]()
+      for (country <- countrySet) {
+        var mov = movieList.filter(_.country.contains(country)).filter(_.vote >= minVotes)
+        mov = mov.sortBy(b => b.duration).reverse
+        finalList+= mov(0)
+      }
+      return finalList
+    }
+
+    var longestMovieList = longestMovieCountryWise(100)
+//    longestMovieList.foreach(println)
   }
+
+
 }
-
-
-
-
